52f6a52fa52f41aec7ddb2234bcabb6c21d8add0
michelecanini <studio.michelecanini@gmail.com>
2023-05-28 11:09:52 +0200

    [UPDATE] adjustment button end docs

--- js/script.js
+++ js/script.js
@@ -7,33 +7,60 @@ PARTE 1 DICHIARAZIONE DELLE VARIABILI
 */
 
 // VARIABILE ARRAY ELENCO IMMAGINI
-let image_slider = [
-    './img/01.webp',
-    './img/02.webp',
-    './img/03.webp',
-    './img/04.webp',
-]
+//let image_slider = [
+//    './img/01.webp',
+//    './img/02.webp',
+//    './img/03.webp',
+//    './img/04.webp',
+//]
 
 // VARIABILE DI COLLEGAMENTO ALLE IMMAGINI SLIDE
 let sliderElement = document.querySelector('.row_slide');
 
-
 // VARIABILE VUOTA PER AGGIUNGERE UN CONTENUTO DEL CAROSELLO
 let slider_container = "";
 
-// CICLO FOR DI SCORRIMENTO SLIDER
+/* 
+PARTE 2 UTILIZZO DEL CICLO FOR PER LO SCORRIMENTO DI TUTTI GLI ELEMENTI IN DELL'ARRAY ELENCO IMMAGINI
+*/
+
+// CICLO FOR DI SCORRIMENTO
 for(let i = 0; i < image_slider.length; i++){
 
     console.log(image_slider[i]);
 
-    // VARIABILE DI RIFERIMENTO
+    // VARIABILE DI RIFERIMENTO CURRENT IMAGE INTERNA AL CICLO
     let current_image = image_slider[i];
+
+    // AGGIUNTA DEL CONTENITORE DIV HTML ALL'INTERNO DI CURRENT_IMAGE
     slider_container += `
         <div class="col_slide">
             <img src="${current_image}" alt="image_one" class="image_one">
         </div>
     `
-    
 }
 
-console.log(slider_container);
+sliderElement.innerHTML = slider_container;
+
+let all_slide = document.getElementsByClassName('col_slide')
+
+
+/* 
+PARTE 3 COLLEGAMENTO AI BOTTONI PREV E NEXT
+
+
+all_slide[0].classList.add('active');
+
+let prevButton = document.querySelector('.button_prev');
+let nextButton = document.querySelector('.button_next');
+
+prevButton.addEventListener('click', function() {
+    console.log("PREV");
+})
+
+nextButton.addEventListener('click', function() {
+    console.log("NEXT");
+}) */
+
+
+
--- readme.md
+++ readme.md
@@ -1,13 +1,14 @@
-# Carosello Mono Array
+# CAROSELLO MONO ARRAY
+## PROBLEMA: Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
 
-## Milestone 1
+### Milestone 1
     1. Creiamo il markup statico costruendo un container.
     2. Inseriamo un'immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
     3. Creare dei container per i bottoni avanti ed indietro.
     4. Centrare ed allineare i bottoni.
     5. Inserire le altre slide per testare il funzionamento.
     
-## Milestone 2
+### Milestone 2
     1. Rimuoviamo tutto il markup statico delle immagini.
     2. inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal.
     3. creiamo una variabile di collegamento alle immagini con querySelector
@@ -15,5 +16,6 @@
     5. creiamo una variabile stringa vuota per lo scorrimento del contenuto immagine nel carosello
     6. inseriamo i div html nel file js all'interno del ciclo for
 
-## Milestone 3
-    1. Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
+### Milestone 3
+    1. al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
+    2. gestire correttamente l'immagine attiva e quelle disattivate.
